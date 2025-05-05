7c996dc9a4e22c348a24d5dc620f6a7716a62368
Shichao Guo <202403888@post.au.dk>
2025-05-05 15:14:07 +0200

    add Door

---
+++ Assets/Room/Door/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    private AudioSource audio;
+    
+    private void Start() {
+        audio = GetComponent<AudioSource>();
+        if (audio == null) {
+            Debug.LogError("AudioSource component not found on the GameObject.");
+        }
+    }
+    
+    public void PlayOpenSound() {
+        if (audio != null) {
+            audio.Play();
+        } else {
+            Debug.LogWarning("AudioSource is not assigned.");
+        }
+    }
+
+}
